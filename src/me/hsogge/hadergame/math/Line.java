9830f2b016cf74f3bf1860d837095979051bdc15
itggot-oskar-wikfeldt <oskar.wikfeldt@itggot.se>
2017-03-27 04:01:49 +0200

    cute

--- src/me/hsogge/hadergame/math/Line.java
+++ src/me/hsogge/hadergame/math/Line.java
@@ -4,11 +4,15 @@ public class Line {
 
     private float x1, y1, x2, y2;
 
+    private float k;
+
     public Line(float x1, float y1, float x2, float y2) {
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
         this.y2 = y2;
+
+        k = (y1 - y2) / (x1 - x2);
     }
 
     public void move(float dx, float dy) {
